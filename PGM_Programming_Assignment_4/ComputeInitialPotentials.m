f9a974f0b90ec11c28de681ace842966ce177fc9
Boris LI <liyandi007@163.com>
2014-11-23 21:37:06 +0800

    Assignment4-2 passed

--- PGM_Programming_Assignment_4/ComputeInitialPotentials.m
+++ PGM_Programming_Assignment_4/ComputeInitialPotentials.m
@@ -39,7 +39,47 @@ P.edges = zeros(N);
 % Print out C to get a better understanding of its structure.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+P.edges = C.edges;
 
+% Compute the cardinality of each variable
+F = C.factorList;
+V = unique([F.var]);
+card = zeros(1, length(V));
+for i = 1 : length(V),
+	for j = 1 : length(F)
+      if (~isempty(find(F(j).var == i)))
+         card(i) = F(j).card(find(F(j).var == i));
+			break;
+		end
+	end
+end
+
+% Assign factors to cliques, satisfying the family property
+factorAssignedToClique = cell(1, N);
+% each cell stores the factors in the clique
+for i = 1:length(C.factorList)
+   for j = 1:N
+      if all(ismember(C.factorList(i).var, C.nodes{j}))
+         factorAssignedToClique{j} = [factorAssignedToClique{j}, i];
+         break; % factor is assigned to only one clique
+      end
+   end
+end
+
+% Compute factor products
+for i = 1:N
+   % init
+   P.cliqueList(i).var = C.nodes{i};
+   P.cliqueList(i).card = card(P.cliqueList(i).var);
+   P.cliqueList(i).val = ones(1, prod(P.cliqueList(i).card));
+   
+   for j = factorAssignedToClique{i}
+      P.cliqueList(i) = FactorProduct(P.cliqueList(i), C.factorList(j));
+   end
+   % reorder vars
+   P.cliqueList(i) = StandardizeFactors(P.cliqueList(i)); 
+
+end
 
 end
 
--- PGM_Programming_Assignment_4/GetNextCliques.m
+++ PGM_Programming_Assignment_4/GetNextCliques.m
@@ -33,7 +33,80 @@ j = 0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% For debugging the code, use GetNextC.INPUT1 as the clique tree
+% and GetNextC.INPUT2 as the message matrix. 
+% The output should be [GetNextC.RESULT1, GetNextC.RESULT2]
 
+% The edge of the clique tree is considered as directed, where one 
+% can pick either (i,j) or (j,i) to be one, and choose a node to be 
+% the root, thus turning it into a directd tree. 
+% The complement order is used later for the reverse message passing.
+% Assume that we simply select the lower triangle to be 
+% the first pass order.
 
+% TARGET CHANGED:
+% Instead of precribing an order, we check by searching smallest node
+% that is activated to pass msg. A node is activated if 
+%   -- it has received msgs from all its neighbors (in edge); or
+%   -- all but one msgs from its neighbors. 
+% In the first case, the receipt can be any neighbor;
+% in the second case, the receipt has to be the one without sending msg,
+% cause otherwise, it has to await for its msg to pass to others.
+% So, in this schema, the two pass phase are interwined, 
+% and algorithm works like an automata.
+
+N = length(P.cliqueList);
+for i = 1:N
+  tmp = 1:N;
+  neighbors = tmp(logical(P.edges(i,:))); %e.g. 1->8, neighbors = [8]
+  
+  % nodes who has sent their msg to i
+  inbox = []; 
+  for k = neighbors
+    if ~isempty(messages(k,i).var)
+      inbox = [inbox, k];
+    end
+  end
+
+  inboxShort = setdiff(neighbors,inbox);
+  % the number of branches awaiting msgs
+  inboxShortSize = length(inboxShort);
+
+  if inboxShortSize >= 2
+    % if more than two incoming msg is missing, the node i cannot be activated
+    continue;
+  end
+
+  % nodes to whom i has sent its msg 
+  outbox = []; 
+  for k = neighbors
+    if ~isempty(messages(i,k).var)
+      outbox = [outbox, k];
+    end
+  end
+  
+  receipts = setdiff(neighbors, outbox); % sorted from small to large 
+  % neighbors still awaiting msg from i = neighbors\(those i has sent a msg)
+  if isempty(receipts)
+    % if all receipts has already got the msg from i, then next
+    continue;
+  end
+
+  % By far, node i has received either all or all but one msgs,
+  % In the first case, we output the smallest neighbor
+  % In the second case, we need to find out which of the receipts hasn't sent yet
+  if inboxShortSize == 0
+    j = min(receipts); % pick the smallest current receipt
+    return; % output i,j
+  elseif ismember(inboxShort,receipts) 
+    % inboxShortSize == 1 and msg not sent yet
+    j = inboxShort;
+    return; % output i,j
+  else 
+    % inboxShortSize == 1 and msg sent already
+    continue;
+  end
+
+end
 
 return;
